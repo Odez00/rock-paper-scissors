57af467b011f2eb1a2b96b8891611cb9661a6238
Odez00 <otto.koskinen@proton.me>
2022-07-22 01:05:29 +0300

    Create function to play

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
+//Generate computers choice
 function getComputerChoice(){
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choice.length);
-    console.log(choice[random])
+    return(choice[random]);
 }
 console.log(getComputerChoice());
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        playRound;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        //computerScore++; 
+        return("You lose! Scissors beat Paper");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        //playerScore++;
+        return("You win! Rock beats Scissors");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        //playerScore++;
+        return("You win! Paper beats Rock");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        //playerScore++;
+        return("You win! Scissors beat Paper");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        //computerScore++;
+        return("You lose! Rock beats Scissors");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        //computerScore++;
+        return("You lose! Paper beats Rock");
+    } else if (playerSelection === computerSelection){
+        return("It's a draw!");
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+playerSelection = "rock";
+computerSelection = getComputerChoice;
+
+console.log(game());
+console.log(playRound(playerSelection, computerSelection()));
